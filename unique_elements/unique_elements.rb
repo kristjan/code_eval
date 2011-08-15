aa4bdd835a29e6a33942c22e07f685b2f14b24ee
Kristján Pétursson <kristjan@gmail.com>
2011-08-15 09:35:10 -0700

    Interesting Unique Elements

--- unique_elements/unique_elements.rb
+++ unique_elements/unique_elements.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/env ruby
 
-def uniqify(numbers)
+def interesting_uniqify(numbers)
+  uniques = []
+  last = nil
+  numbers.each do |n|
+    unless n == last
+      uniques << n
+      last = n
+    end
+  end
+  uniques
+end
+
+def simple_uniqify(numbers)
   numbers.uniq.sort
 end
 
 File.readlines(ARGV[0]).each do |line|
   numbers = line.strip.split(',')
-  unique = uniqify(numbers)
-  puts unique.join(',')
+  uniques = interesting_uniqify(numbers)
+  puts uniques.join(',')
 end
