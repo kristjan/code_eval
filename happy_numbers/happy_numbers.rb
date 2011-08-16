aa9d266aca2f1c03d8bff80bcf45763057052cbc
Kristján Pétursson <kristjan@gmail.com>
2011-08-15 21:12:54 -0700

    Happy Numbers

---
+++ happy_numbers/happy_numbers.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+class Fixnum
+  def digits
+    self.to_s.split('').map(&:to_i)
+  end
+
+  def happy_number?
+    seen = Set.new
+    n = self
+    while true
+      return true if n == 1
+      return false if seen.include?(n)
+      seen << n
+      squares = n.digits.map{|d| d * d}
+      n = squares.inject(:+)
+    end
+  end
+end
+
+File.readlines(ARGV[0]).each do |line|
+  puts line.to_i.happy_number? ? 1 : 0
+end
