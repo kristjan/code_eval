a0d395aaa80c46073dbdaad560678fdaee6fcdf7
Kristján Pétursson <kristjan@gmail.com>
2011-08-17 22:24:39 -0700

    Simpler Detect Cycles solution
    Since no one asked for a fancy version

--- detecting_cycles/detecting_cycles.rb
+++ detecting_cycles/detecting_cycles.rb
@@ -1,37 +1,57 @@
 #!/usr/bin/env ruby
 
-def build_graph(sequence)
-  {}.tap do |graph|
-    sequence.each_cons(2) do |i, j|
-      graph[i] = j
+module Simple
+  class << self
+    def detect_cycle(sequence)
+      locations = {}
+      sequence.each_with_index do |n, i|
+        if locations[n]
+          return sequence[locations[n]...i]
+        else
+          locations[n] = i
+        end
+      end
+      nil
     end
   end
 end
 
-def build_cycle_from(graph, n)
-  [n].tap do |cycle|
-    cycle << graph[cycle.last] until graph[cycle.last] == cycle.first
-  end
-end
+module TortiseAndHare
+  class << self
+    def build_graph(sequence)
+      {}.tap do |graph|
+        sequence.each_cons(2) do |i, j|
+          graph[i] = j
+        end
+      end
+    end
 
-def gather_cycle(sequence, graph, n)
-  cycle = build_cycle_from(graph, n)
-  leftmost = sequence.find{|i| cycle.include?(i)}
-  build_cycle_from(graph, leftmost)
-end
+    def build_cycle_from(graph, n)
+      [n].tap do |cycle|
+        cycle << graph[cycle.last] until graph[cycle.last] == cycle.first
+      end
+    end
 
-def detect_cycle(sequence)
-  graph = build_graph(sequence)
-  sluggish, speedy = sequence[0], sequence[1]
-  until speedy.nil?
-    return gather_cycle(sequence, graph, speedy) if speedy == sluggish
-    sluggish = graph[sluggish]
-    speedy = graph[graph[speedy]]
+    def gather_cycle(sequence, graph, n)
+      cycle = build_cycle_from(graph, n)
+      leftmost = sequence.find{|i| cycle.include?(i)}
+      build_cycle_from(graph, leftmost)
+    end
+
+    def detect_cycle(sequence)
+      graph = build_graph(sequence)
+      tortise, hare = sequence[0], sequence[1]
+      until hare.nil?
+        return gather_cycle(sequence, graph, hare) if hare == tortise
+        tortise = graph[tortise]
+        hare = graph[graph[hare]]
+      end
+      nil
+    end
   end
-  nil
 end
 
 File.readlines(ARGV[0]).each do |line|
-  cycle = detect_cycle(line.split(" ").map(&:to_i))
+  cycle = Simple.detect_cycle(line.split(" ").map(&:to_i))
   puts cycle.join(' ') unless cycle.nil?
 end
