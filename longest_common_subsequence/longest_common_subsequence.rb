26f57de72c610e6e59ce3bdeaa7a8335850a3989
Kristján Pétursson <kristjan@gmail.com>
2011-08-23 00:04:29 -0700

    Fast Longest Common Subsequence
    Faster languages have nothing on faster approaches.

--- longest_common_subsequence/longest_common_subsequence.rb
+++ longest_common_subsequence/longest_common_subsequence.rb
@@ -1,17 +1,42 @@
 #!/usr/bin/env ruby
 
-def longest_common_subsequence(a, b)
-  return '' if a.empty? || b.empty?
-  if a[0] == b[0]
-    a[0] + longest_common_subsequence(a[1..-1], b[1..-1])
-  else
-    drop_a = longest_common_subsequence(a[1..-1], b)
-    drop_b = longest_common_subsequence(a, b[1..-1])
-    [drop_a, drop_b].sort_by(&:length).last
+def size_grid(a, b)
+  sizes = 0.upto(a.length).map{Array.new()}
+  0.upto(a.length) {|ai| sizes[ai][b.length] = 0}
+  0.upto(b.length) {|bi| sizes[a.length][bi] = 0}
+
+  (a.length - 1).downto(0) do |ai|
+    (b.length - 1).downto(0) do |bi|
+      if a[ai] == b[bi]
+        sizes[ai][bi] = 1 + sizes[ai + 1][bi + 1]
+      else
+        sizes[ai][bi] = [sizes[ai + 1][bi], sizes[ai][bi + 1]].max
+      end
+    end
   end
+  sizes
+end
+
+def longest_common_subsequence(a, b)
+  sizes = size_grid(a, b)
+  lcs = ''
+  ai = bi = 0
+  while ai < a.length && bi < b.length
+    if a[ai] == b[bi]
+      lcs << a[ai]
+      ai += 1
+      bi += 1
+    elsif sizes[ai + 1][bi] > sizes[ai][bi + 1]
+      ai += 1
+    else
+      bi += 1
+    end
+  end
+  lcs
 end
 
 File.readlines(ARGV[0]).each do |line|
+  next if line.empty?
   a, b = line.split(';')
-  puts longest_common_subsequence(a, b)
+  puts longest_common_subsequence(a || '', b || '')
 end
