a850629f0ac0c75d03f85990dc8e9998b47de660
Kristján Pétursson <kristjan@gmail.com>
2011-08-26 00:07:38 -0700

    Refactor Message Decode
    `Message.decode` was pretty dense. This is a bit more code, but `Stream`
    provides a nice API that simplifies the top level.
    
    The `Dictionary` class doesn't simplify anything, but it does get the key/value
    generation out of `Message`, where it doesn't quite belong.
    
    My only real gripe is that `Stream` awkwardly looks ahead one character, which
    was necessary to be able to register `empty?` before a `nil` got returned. On
    the other hand, it DRYs up the segment and character loops that existed in
    `decode`.

--- message_decoding/message_decoding.rb
+++ message_decoding/message_decoding.rb
@@ -1,50 +1,81 @@
 #!/usr/bin/env ruby
 
 module Message
-  class << self
-    def decode(str)
-      header, message = $1, $2 if str =~ /^([^01]+)([01]+)$/
-      dict = dictionary(header)
-      bits = message.split('')
-      "".tap do |result|
-        segment_length = get_segement_length(bits)
-        while segment_length > 0
-          terminator = '1' * segment_length
-          key = get_encoded_character(bits, segment_length)
-          while key != terminator
-            result << dict[key]
-            key = get_encoded_character(bits, segment_length)
-          end
-          segment_length = get_segement_length(bits)
-        end
-      end
-    end
-
-    def dictionary(header)
-      {}.tap do |dict|
-        segment_length = 1
-        key = 0
-        header.chars.each do |ch|
-          dict[key.to_s(2).rjust(segment_length, '0')] = ch
-          key += 1
-          if key == 2**segment_length - 1
-            segment_length += 1
-            key = 0
-          end
-        end
-      end
-    end
-
-    private
-
-    def get_encoded_character(bits, segment_length)
-      bits.shift(segment_length).join
-    end
-
-    def get_segement_length(bits)
-      bits.shift(3).join.to_i(2)
+  def self.decode(str)
+    header, message = $1, $2 if str =~ /^([^01]+)([01]+)$/
+    dict = Dictionary.new(header)
+    stream = Stream.new(message)
+    "".tap do |result|
+      result << dict[stream.next_key] until stream.empty?
     end
   end
+
+private
+
+  class Dictionary
+    def initialize(header)
+      @header = header
+      build
+    end
+
+    def [](key)
+      @dictionary[key]
+    end
+
+  private
+
+    def build
+      @dictionary = {}
+      segment_length = 1
+      key = 0
+      @header.chars.each do |ch|
+        @dictionary[key.to_s(2).rjust(segment_length, '0')] = ch
+        key += 1
+        if key == 2**segment_length - 1
+          segment_length += 1
+          key = 0
+        end
+      end
+    end
+  end
+
+  class Stream
+    def initialize(message)
+      @bits = message.split('')
+      @segment_length = next_segment_length
+      @pending_key = read
+    end
+
+    def empty?
+      @bits.empty?
+    end
+
+    def next_key
+      return if empty?
+      key = @pending_key
+      @pending_key = read
+      if @pending_key == terminator
+        @segment_length = next_segment_length
+        @pending_key = read unless @segment_length == 0
+      end
+      key
+    end
+
+  private
+
+    def terminator
+      '1' * @segment_length
+    end
+
+    def next_segment_length
+      @bits.shift(3).join.to_i(2)
+    end
+
+    def read
+      @bits.shift(@segment_length).join
+    end
+  end
+
 end
 
 File.readlines(ARGV[0]).each do |line|
